9da77331fb7a044190badab868ac5b1ebdd1c0a1
AmmarAhmed <ammarahmed1263@gmail.com>
2025-09-20 16:48:13 +0300

    enhance ui and nav experience

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-wrapper ul");
+const navItems = document.querySelector(".nav-wrapper ul");
+const navLinks = document.querySelectorAll(".nav-wrapper ul li a");
 const filterButtons = document.querySelectorAll(".filter-btn");
 const projects = document.querySelectorAll(".project-card");
+const overlay = document.querySelector(".overlay");
 
 const filterProjects = (filter) => {
-    projects.forEach((project) => {
-      project.classList.remove("hidden");
-      if (
-        filter !== "all" &&
-        !project.getAttribute("data-category").includes(filter)
-      ) {
-        project.classList.add("hidden");
-      }
-    })
-}
+  projects.forEach((project) => {
+    project.classList.remove("hidden");
+    if (
+      filter !== "all" &&
+      !project.getAttribute("data-category").includes(filter)
+    ) {
+      project.classList.add("hidden");
+    }
+  });
+};
 
 menuToggle.addEventListener("click", () => {
   const expanded = menuToggle.getAttribute("aria-expanded") === "true";
   menuToggle.setAttribute("aria-expanded", String(!expanded));
-  navLinks.classList.toggle("show");
-  console.log('nav links', navLinks.classList);
+  overlay.classList.toggle("active");
+  navItems.classList.toggle("show");
+});
+
+overlay.addEventListener("click", () => {
+  navItems.classList.remove("show");
+  overlay.classList.remove("active");
+  menuToggle.setAttribute("aria-expanded", "false");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    console.log("nav links", navLinks, link);
+    navItems.classList.remove("show");
+    overlay.classList.remove("active");
+    menuToggle.setAttribute("aria-expanded", "false");
+  });
 });
 
 filterButtons.forEach((button) => {
